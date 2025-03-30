24d212fb63fad3311365ba1f66efdb9c1313bdc9
Asriel Camora <asriel@camora.dev>
2025-03-30 02:42:30 -0700

    Better schema defaults for unknown sheets

--- viewer/src/schema/format.rs
+++ viewer/src/schema/format.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use jsonschema::{
     BasicOutput,
     output::{ErrorDescription, OutputUnit},
@@ -12,24 +13,36 @@ use std::{
 #[serde(rename_all = "camelCase")]
 pub struct Schema {
     pub name: String,
+    #[serde(skip_serializing_if = "is_default")]
     pub display_field: Option<String>,
     pub fields: Vec<Field>,
+    #[serde(skip_serializing_if = "is_default")]
     pub pending_fields: Option<Vec<Field>>,
+    #[serde(skip_serializing_if = "is_default")]
     pub relations: Option<HashMap<String, Vec<String>>>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct Field {
+    #[serde(skip_serializing_if = "is_default")]
     pub name: Option<String>,
+    #[serde(skip_serializing_if = "is_default")]
     pub pending_name: Option<String>,
     #[serde(default)]
+    #[serde(skip_serializing_if = "is_default")]
     pub r#type: FieldType,
+    #[serde(skip_serializing_if = "is_default")]
     pub count: Option<u32>,
+    #[serde(skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+    #[serde(skip_serializing_if = "is_default")]
     pub fields: Option<Vec<Field>>,
+    #[serde(skip_serializing_if = "is_default")]
     pub relations: Option<HashMap<String, Vec<String>>>,
+    #[serde(skip_serializing_if = "is_default")]
     pub condition: Option<Condition>,
+    #[serde(skip_serializing_if = "is_default")]
     pub targets: Option<Vec<String>>,
 }
 
@@ -45,11 +58,15 @@ pub enum FieldType {
     Color,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Condition {
     pub switch: String,
     pub cases: HashMap<i32, Vec<String>>,
+}
+
+fn is_default<T: Default + Eq>(value: &T) -> bool {
+    value == &T::default()
 }
 
 static SCHEMA: LazyLock<jsonschema::Validator> = LazyLock::new(|| {
@@ -79,6 +96,38 @@ impl Schema {
                 }
                 Ok(Err(errors))
             }
+        }
+    }
+
+    pub fn from_blank(name: impl Into<String>, column_count: usize) -> Self {
+        Self {
+            name: name.into(),
+            fields: (0..column_count)
+                .map(|i| Field {
+                    name: Some(format!("Unknown{i}")),
+                    ..Default::default()
+                })
+                .collect_vec(),
+            ..Default::default()
+        }
+    }
+
+    pub fn misc_sheet(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            fields: vec![
+                Field {
+                    name: Some("Key".to_string()),
+                    r#type: FieldType::Scalar,
+                    ..Default::default()
+                },
+                Field {
+                    name: Some("Value".to_string()),
+                    r#type: FieldType::Scalar,
+                    ..Default::default()
+                },
+            ],
+            ..Default::default()
         }
     }
 }
