05407faf43110a52b7b18b000096b6447806c84f
Asriel Camora <asriel@camora.dev>
2025-05-06 13:46:03 -0700

    Fix native build

--- viewer/src/schema/boxed.rs
+++ viewer/src/schema/boxed.rs
@@ -3,6 +3,7 @@ use super::{cache::CachedProvider, provider::SchemaProvider};
 pub type BoxedSchemaProvider = CachedProvider<Box<dyn SchemaProvider>>;
 
 impl BoxedSchemaProvider {
+    #[cfg(not(target_arch = "wasm32"))]
     pub fn new_local(value: super::local::LocalProvider) -> Self {
         CachedProvider::new(
             Box::new(value) as Box<dyn SchemaProvider>,
