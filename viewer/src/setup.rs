1d06abb56b1b32bf0f455e433a4ca9fd13e81140
Asriel Camora <asriel@camora.dev>
2025-05-06 16:21:17 -0700

    Refactor and add history entries on web

--- viewer/src/setup.rs
+++ viewer/src/setup.rs
@@ -1,17 +1,18 @@
-use egui::{Frame, Vec2, WidgetText};
+use egui::{ComboBox, Frame, Layout, TextEdit, Vec2, WidgetText};
 
 use crate::{
     DEFAULT_API_URL, DEFAULT_SCHEMA_URL,
     data::{AppConfig, InstallLocation, SchemaLocation},
+    utils::TrackedPromise,
 };
 
 pub struct SetupWindow {
     location: InstallLocation,
     schema: SchemaLocation,
     #[cfg(target_arch = "wasm32")]
-    install_folder_promise: Option<crate::utils::TrackedPromise<anyhow::Result<String>>>,
+    location_promises: SetupPromises,
     #[cfg(target_arch = "wasm32")]
-    schema_folder_promise: Option<crate::utils::TrackedPromise<anyhow::Result<String>>>,
+    schema_promises: SetupPromises,
 }
 
 impl Default for SetupWindow {
@@ -28,9 +29,9 @@ impl Default for SetupWindow {
             location,
             schema: SchemaLocation::Web(super::DEFAULT_SCHEMA_URL.to_string()),
             #[cfg(target_arch = "wasm32")]
-            install_folder_promise: None,
+            location_promises: Default::default(),
             #[cfg(target_arch = "wasm32")]
-            schema_folder_promise: None,
+            schema_promises: Default::default(),
         }
     }
 }
@@ -45,9 +46,9 @@ impl SetupWindow {
             location: config.location,
             schema: config.schema,
             #[cfg(target_arch = "wasm32")]
-            install_folder_promise: None,
+            location_promises: Default::default(),
             #[cfg(target_arch = "wasm32")]
-            schema_folder_promise: None,
+            schema_promises: Default::default(),
         }
     }
 
@@ -58,28 +59,12 @@ impl SetupWindow {
     ) -> Option<AppConfig> {
         #[cfg(target_arch = "wasm32")]
         {
-            if let Some(result) = self.install_folder_promise.take_if(|p| p.poll().is_ready()) {
-                let result = poll_promise::Promise::from(result).block_and_take();
-                match result {
-                    Ok(path) => {
-                        self.location = InstallLocation::Worker(path);
-                    }
-                    Err(e) => {
-                        log::error!("Error picking folder: {e}");
-                    }
-                }
+            if let Some(path) = self.location_promises.take_folder() {
+                self.location = InstallLocation::Worker(path);
             }
 
-            if let Some(result) = self.schema_folder_promise.take_if(|p| p.poll().is_ready()) {
-                let result = poll_promise::Promise::from(result).block_and_take();
-                match result {
-                    Ok(path) => {
-                        self.schema = SchemaLocation::Worker(path);
-                    }
-                    Err(e) => {
-                        log::error!("Error picking folder: {e}");
-                    }
-                }
+            if let Some(path) = self.schema_promises.take_folder() {
+                self.schema = SchemaLocation::Worker(path);
             }
         }
 
@@ -138,15 +123,20 @@ impl SetupWindow {
                             InstallLocation::Sqpack(path) => {
                                 ui.horizontal(|ui| {
                                     ui.label("Path:");
-                                    ui.text_edit_singleline(path);
-                                    if ui.button("...").clicked() {
-                                        if let Some(picked_path) = rfd::FileDialog::new()
-                                            .pick_folder()
-                                            .and_then(|d| d.to_str().map(|s| s.to_owned()))
+                                    ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                                        if ui
+                                            .button("Browse")
+                                            .clicked()
                                         {
-                                            *path = picked_path;
+                                            if let Some(picked_path) = rfd::FileDialog::new()
+                                                .pick_folder()
+                                                .and_then(|d| d.to_str().map(|s| s.to_owned()))
+                                            {
+                                                *path = picked_path;
+                                            }
                                         }
-                                    }
+                                        ui.add(TextEdit::singleline(path).desired_width(ui.available_width()));
+                                    });
                                 });
                             }
 
@@ -154,15 +144,45 @@ impl SetupWindow {
                             InstallLocation::Worker(name) => {
                                 ui.horizontal(|ui| {
                                     ui.label("Name:");
-                                    ui.text_edit_singleline(&mut name.as_str());
-                                    if ui.button("...").clicked() {
-                                        self.install_folder_promise =
-                                            Some(Self::open_folder_picker(ui.ctx().clone(),
-                                            web_sys::FileSystemPermissionMode::Read,
-                                                |handle| {
-                                                        crate::excel::worker::WorkerFileProvider::add_folder(handle)
-                                                }));
-                                    }
+                                    ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                                        if ui.button("Browse").clicked() {
+                                            self.location_promises.open_folder_picker(
+                                                ui.ctx().clone(),
+                                                web_sys::FileSystemPermissionMode::Read,
+                                                crate::excel::worker::WorkerFileProvider::add_folder,
+                                            );
+                                        }
+                                        ComboBox::from_id_salt("install_folder")
+                                            .selected_text(name.as_str())
+                                            .width(ui.available_width())
+                                            .show_ui(ui, |ui| {
+                                                match self.location_promises.get_folder_list(
+                                                ui.ctx().clone(),
+                                                crate::excel::worker::WorkerFileProvider::folders,
+                                            ) {
+                                                None => {
+                                                    ui.label("Retrieving...");
+                                                }
+                                                Some(Err(e)) => {
+                                                    ui.label(format!("An error occured: {e}"));
+                                                }
+                                                Some(Ok(entries)) => {
+                                                    if entries.is_empty() {
+                                                        ui.label("None");
+                                                    }
+                                                    else{
+                                                    for entry in entries {
+                                                        ui.selectable_value(
+                                                            name,
+                                                            entry.to_string(),
+                                                            entry,
+                                                        );
+                                                    }
+                                                }
+                                                }
+                                            }
+                                        });
+                                    });
                                 });
                             }
 
@@ -207,8 +227,11 @@ impl SetupWindow {
                             SchemaLocation::Local(path) => {
                                 ui.horizontal(|ui| {
                                     ui.label("Path:");
-                                    ui.text_edit_singleline(path);
-                                    if ui.button("...").clicked() {
+                                    ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                                    if ui
+                                        .button("Browse")
+                                        .clicked()
+                                    {
                                         if let Some(picked_path) = rfd::FileDialog::new()
                                             .pick_folder()
                                             .and_then(|d| d.to_str().map(|s| s.to_owned()))
@@ -216,6 +239,8 @@ impl SetupWindow {
                                             *path = picked_path;
                                         }
                                     }
+                                    ui.add(TextEdit::singleline(path).desired_width(ui.available_width()));
+                                });
                                 });
                             }
 
@@ -223,16 +248,47 @@ impl SetupWindow {
                             SchemaLocation::Worker(name) => {
                                 ui.horizontal(|ui| {
                                     ui.label("Name:");
-                                    ui.text_edit_singleline(&mut name.as_str());
-                                    if ui.button("...").clicked() {
-                                        self.schema_folder_promise =
-                                            Some(Self::open_folder_picker(ui.ctx().clone(), 
+                                    ui.with_layout(Layout::right_to_left(egui::Align::Min), |ui| {
+                                        if ui
+                                        .button("Browse")
+                                        .clicked() {
+                                            self.schema_promises.open_folder_picker(
+                                                ui.ctx().clone(),
                                                 web_sys::FileSystemPermissionMode::Readwrite,
-                                                |handle| {
-                                                        crate::schema::worker::WorkerProvider::add_folder(handle)
-                                                },
-                                            ));
-                                    }
+                                                crate::schema::worker::WorkerProvider::add_folder,
+                                            );
+                                        }
+                                        ComboBox::from_id_salt("schema_folder")
+                                        .selected_text(name.as_str())
+                                        .width(ui.available_width())
+                                        .show_ui(ui, |ui| {
+                                            match self.schema_promises.get_folder_list(
+                                                ui.ctx().clone(),
+                                                crate::schema::worker::WorkerProvider::folders,
+                                            ) {
+                                                None => {
+                                                    ui.label("Retrieving...");
+                                                }
+                                                Some(Err(e)) => {
+                                                    ui.label(format!("An error occured: {e}"));
+                                                }
+                                                Some(Ok(entries)) => {
+                                                    if entries.is_empty() {
+                                                        ui.label("None");
+                                                    }
+                                                    else {
+                                                        for entry in entries {
+                                                            ui.selectable_value(
+                                                                name,
+                                                                entry.to_string(),
+                                                                entry,
+                                                            );
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        });
+                                    });
                                 });
                             }
 
@@ -263,19 +319,56 @@ impl SetupWindow {
             None
         }
     }
+}
 
-    #[cfg(target_arch = "wasm32")]
+fn radio(ui: &mut egui::Ui, selected: bool, text: impl Into<WidgetText>) -> bool {
+    let mut resp = ui.radio(selected, text);
+    if resp.clicked() && !selected {
+        resp.mark_changed();
+        true
+    } else {
+        false
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[derive(Default)]
+struct SetupPromises {
+    selected: Option<TrackedPromise<anyhow::Result<String>>>,
+    list: Option<TrackedPromise<anyhow::Result<Vec<String>>>>,
+}
+
+#[cfg(target_arch = "wasm32")]
+impl SetupPromises {
+    fn take_folder(&mut self) -> Option<String> {
+        if let Some(result) = self.selected.take_if(|p| p.poll().is_ready()) {
+            let result = poll_promise::Promise::from(result).block_and_take();
+
+            self.list.take();
+            match result {
+                Ok(path) => Some(path),
+                Err(e) => {
+                    log::error!("Error picking folder: {e}");
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+
     fn open_folder_picker<F: Future<Output = anyhow::Result<String>>>(
+        &mut self,
         ctx: egui::Context,
         mode: web_sys::FileSystemPermissionMode,
         store_folder: impl Fn(web_sys::FileSystemDirectoryHandle) -> F + 'static,
-    ) -> crate::utils::TrackedPromise<anyhow::Result<String>> {
+    ) {
         use anyhow::anyhow;
         use eframe::wasm_bindgen::JsCast;
         use wasm_bindgen_futures::JsFuture;
         use web_sys::{DirectoryPickerOptions, FileSystemDirectoryHandle};
 
-        crate::utils::TrackedPromise::spawn_local(ctx, async move {
+        let ret = crate::utils::TrackedPromise::spawn_local(ctx, async move {
             let opts = DirectoryPickerOptions::new();
             opts.set_mode(mode);
             let promise = web_sys::window()
@@ -292,16 +385,20 @@ impl SetupWindow {
                 }
                 Err(e) => Err(anyhow!("Error picking folder: {e:?}")),
             }
-        })
+        });
+        self.selected = Some(ret);
     }
-}
 
-fn radio(ui: &mut egui::Ui, selected: bool, text: impl Into<WidgetText>) -> bool {
-    let mut resp = ui.radio(selected, text);
-    if resp.clicked() && !selected {
-        resp.mark_changed();
-        true
-    } else {
-        false
+    fn get_folder_list<F: Future<Output = anyhow::Result<Vec<String>>> + 'static>(
+        &mut self,
+        ctx: egui::Context,
+        future: impl FnOnce() -> F,
+    ) -> Option<&anyhow::Result<Vec<String>>> {
+        if self.list.is_none() {
+            self.list = Some(TrackedPromise::spawn_local(ctx, future()));
+        }
+        let promise = self.list.as_deref().unwrap();
+
+        promise.ready()
     }
 }
