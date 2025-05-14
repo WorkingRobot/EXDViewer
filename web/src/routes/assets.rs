32a671dd7523e0cfd5b7f07ffa7f4cf5bedabab6
Asriel Camora <asriel@camora.dev>
2025-05-14 16:11:33 -0700

    Fix SPA

--- web/src/routes/assets.rs
+++ web/src/routes/assets.rs
@@ -1,23 +1,27 @@
-use std::env::{current_dir, current_exe};
+use std::{
+    env::{current_dir, current_exe},
+    path::PathBuf,
+    sync::LazyLock,
+};
 
-use actix_files::Files;
+use actix_files::{Files, NamedFile};
 use actix_web::{dev::HttpServiceFactory, get};
+
+static SERVICE_DIRECTORY: LazyLock<PathBuf> = LazyLock::new(|| {
+    current_exe()
+        .map(|p| p.parent().map(|p| p.to_path_buf()).unwrap_or(p))
+        .unwrap_or_else(|_| current_dir().unwrap())
+        .join("static")
+});
 
 pub fn service() -> impl HttpServiceFactory {
     (
         index,
-        Files::new(
-            "/",
-            current_exe()
-                .map(|p| p.parent().map(|p| p.to_path_buf()).unwrap_or(p))
-                .unwrap_or_else(|_| current_dir().unwrap())
-                .join("static"),
-        )
-        .index_file("index.html"),
+        Files::new("/", SERVICE_DIRECTORY.clone()).index_file("index.html"),
     )
 }
 
 #[get("/{tail:[^\\.]+}")]
-async fn index(path: web::Data<PathBuf>) -> actix_web::Result<NamedFile> {
-    Ok(NamedFile::open(path.join("index.html"))?)
+async fn index() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open(SERVICE_DIRECTORY.join("index.html"))?)
 }
